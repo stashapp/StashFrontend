560d34f538eb8dec666120696cd8dbb0c266989b
Stash Dev <stashappdev@gmail.com>
2017-08-11 12:46:40 -0700

    Warning fix

--- src/app/scenes/scenes.service.ts
+++ src/app/scenes/scenes.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { SceneListState } from '../shared/models/list-state.model';
 
-import { TimerObservable } from "rxjs/Observable/TimerObservable";
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
 import * as localForage from 'localforage';
 
 @Injectable()
